322827077c69a41e3d06c417827487c177f59522
Greeshma <greeshma.kannegolla@remotestar.io>
2022-01-29 17:24:54 +0530

    Image box fit added UI improved

--- milkman_assessment/lib/restaurantCard.dart
+++ milkman_assessment/lib/restaurantCard.dart
@@ -20,77 +20,89 @@ class _RestaurantCardState extends State<RestaurantCard> {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(20.0),
       ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 10),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ClipRRect(
-              borderRadius: const BorderRadius.all(Radius.circular(20)),
-              child: SizedBox(
-                height: 230,
-                width: double.infinity,
-                child: Image.asset(
-                    "images/food1.jpeg"), //TODO:Get each restaurant's name
-              ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: const BorderRadius.only(
+                topRight: Radius.circular(20), topLeft: Radius.circular(20)),
+            child: SizedBox(
+              height: 230,
+              width: double.infinity,
+              child: Image.asset("images/food1.jpeg",
+                  fit: BoxFit.fill), //TODO:Get each restaurant's name
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 16.0, bottom: 5.5),
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 4),
+            child: _getRestaurantInfo(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Column _getRestaurantInfo() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 8.0, bottom: 5.5),
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Text(
+              "Paradise",
+              style: kHeader,
+            ),
+            ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(8)),
+              child: Container(
+                color: ColorConstants.ratingColor,
+                height: 25,
+                width: 45,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 5.0, vertical: 3),
+                  child: Row(children: [
                     Text(
-                      "Paradise",
-                      style: kHeader,
+                      "3.5",
+                      style: kSecondaryHeader.copyWith(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 13.5,
+                          color: ColorConstants.appBackgroundColor),
                     ),
-                    ClipRRect(
-                      borderRadius: const BorderRadius.all(Radius.circular(8)),
-                      child: Container(
-                        color: ColorConstants.ratingColor,
-                        height: 25,
-                        width: 45,
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 5.0, vertical: 3),
-                          child: Row(children: [
-                            Text(
-                              "3.5",
-                              style: kSecondaryHeader.copyWith(
-                                  fontWeight: FontWeight.w600,
-                                  fontSize: 13.5,
-                                  color: ColorConstants.appBackgroundColor),
-                            ),
-                            SizedBox(
-                              width: 2,
-                            ),
-                            Icon(
-                              Icons.star_rounded,
-                              size: 12,
-                              color: ColorConstants.appBackgroundColor,
-                            )
-                          ]),
-                        ),
-                      ),
+                    SizedBox(
+                      width: 2,
+                    ),
+                    Icon(
+                      Icons.star_rounded,
+                      size: 12,
+                      color: ColorConstants.appBackgroundColor,
                     )
                   ]),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: const [
-                Text("South Indian, Chinese", style: kSecondaryHeader),
-                Text("₹300 for one", style: kSecondaryHeader)
-              ],
-            ),
-            SizedBox(
-              height: 5.5,
-            ),
-            Text("Closes in 25 mins",
-                style: kSecondaryHeader.copyWith(
-                    color: ColorConstants.appPrimaryColor)),
+                ),
+              ),
+            )
+          ]),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: const [
+            Text("South Indian, Chinese", style: kSecondaryHeader),
+            Text("₹300 for one", style: kSecondaryHeader)
           ],
         ),
-      ),
+        SizedBox(
+          height: 5.5,
+        ),
+        Text("Closes in 25 mins",
+            style: kSecondaryHeader.copyWith(
+                color: ColorConstants.appPrimaryColor)),
+        SizedBox(
+          height: 5.5,
+        ),
+      ],
     );
   }
 }
